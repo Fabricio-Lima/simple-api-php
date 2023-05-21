1ae03a991198f02613760b20fe7a7d115d2fdb10
Fabricio-Lima <fabriciolc61@gmail.com>
2023-05-21 03:24:52 -0300

    feat: :wrench: Database
    Configura arquivo de conexao com banco de dados

---
+++ src/System/DatabaseConnector.php
@@ -0,0 +1,31 @@
+<?php
+namespace Src\System;
+
+class DatabaseConnector {
+
+    private $dbConnection = null;
+
+    public function __construct()
+    {
+        $host = $_ENV['DB_HOST'];
+        $port = $_ENV['DB_PORT'];
+        $db   = $_ENV['DB_DATABASE'];
+        $user = $_ENV['DB_USERNAME'];
+        $pass = $_ENV['DB_PASSWORD'];
+
+        try {
+            $this->dbConnection = new \PDO(
+                "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$db",
+                $user,
+                $pass
+            );
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->dbConnection;
+    }
+}
